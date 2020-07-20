8fb9b715b0a0a8b24a37af07f78fd88f04958a69
Vincent-Garret <vincentgarret33@gmail.com>
2020-07-20 17:49:06 +0200

    contactForm

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,38 @@
 namespace App\Controller;
 
 
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param ContactRepository $contactRepository
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function home(){
-        return $this->render('front/index.html.twig');
+    public function home(Request $request,
+                         EntityManagerInterface $entityManager,
+                         ContactRepository $contactRepository)
+    {
+        $contact = new Contact();
+        $contactForm = $this->createForm(ContactType::class, $contact);
+
+        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+            // je persiste et je flush
+            $entityManager->persist($contact);
+            $entityManager->flush();
+        }
+        return $this->render('front/index.html.twig',[
+            'contactForm' => $contactForm->createView()]);
     }
 
 }
