ebf552fea81e5a9c7b40355f6f43dc1f0269f475
Vincent-Garret <vincentgarret33@gmail.com>
2020-07-25 23:29:01 +0200

    mailer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,11 +20,13 @@ class HomeController extends AbstractController
      * @param Request $request
      * @param EntityManagerInterface $entityManager
      * @param ContactRepository $contactRepository
+     * @param \Swift_Mailer $mailer
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function home(Request $request,
                          EntityManagerInterface $entityManager,
-                         ContactRepository $contactRepository)
+                         ContactRepository $contactRepository,
+                         \Swift_Mailer $mailer)
     {
         $contact = new Contact();
         $contactForm = $this->createForm(ContactType::class, $contact);
@@ -35,6 +37,35 @@ class HomeController extends AbstractController
             // je persiste et je flush
             $entityManager->persist($contact);
             $entityManager->flush();
+            $mail = $contactForm['mail']->getData();
+            $message = $contactForm['message']->getData();
+            $name = $contactForm['Name']->getData();
+            $phone = $contactForm['telephone']->getData();
+
+            // MAILER
+            $mail = (new \Swift_Message('Votre demande de contact'))
+                ->setFrom('vincent@vamweb.fr')
+                ->setTo($mail)
+                ->setSubject('Votre demande de contact')
+                ->setBody($this->renderView('mail/frontMail.html.twig',
+                    ['name' => $name,
+                        'phone' => $phone,
+                        'message' => $message
+                    ]));
+
+            $autoMail =( new \Swift_Message('Nouvelle demande de contact'))
+                ->setFrom('bistrotgirondin@gmail.com')
+                ->setTo('bistrotgirondin@gmail.com')
+                ->setSubject('Nouvelle demande de contact')
+                ->setBody($this->renderView('mail/adminMail.html.twig',
+                    ['name' => $name,
+                        'phone' => $phone,
+                        'message' => $message
+                    ]));
+
+            $this->addFlash('success', 'Votre demande de contact à bien été envoyée !');
+            $mailer->send($mail);
+            $mailer->send($autoMail);
         }
         return $this->render('front/index.html.twig',[
             'contactForm' => $contactForm->createView()]);
